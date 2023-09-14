1b886818955523a573ffb8e382ff1e9bf24f1425
Christopher Brum <christopher.brum@gmail.com>
2023-09-13 18:30:25 -0700

    Co-authored-by: MADDY WOODRUM madzno12@gmail.com Co-authored-by: LEEYA DAVIS leeya463@gmail.com Co-authored-by: SPENCDER WIGHTMAN spencer.wightman@gmail.com

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,116 +1,117 @@
-import {useState} from "react";
+import { useState, useEffect } from "react";
 import { Table } from 'react-bootstrap'
+import requestService from './services/requests'
 
-const mongoRequestsData = [
-  {
-    id: 1,
-    requestHeaders: {
-      'host': 'requestbinder.com/1111',
-      'content-length': '97',
-      'accept': 'applic... */*',
-      'sec-fetch-site': 'same-origin',
-      'accept-language': 'en-GB,en;q=0.9',
-      'accept-encoding': 'gzip, deflate, br',
-      'sec-fetch-mode': 'cors',
-      'content-type': 'application/json',
-      'origin': 'https:....com',
-      'user-agent': 'Mozill...1.15',
-      'referer': 'https:...eea1',
-      'sec-fetch-dest': 'empty',
-    }
-  },
-  {
-    id: 2,
-    requestHeaders: {
-      'host': 'requestbinder.com/2222',
-      'content-length': '97',
-      'accept': 'applic... */*',
-      'sec-fetch-site': 'same-origin',
-      'accept-language': 'en-GB,en;q=0.9',
-      'accept-encoding': 'gzip, deflate, br',
-      'sec-fetch-mode': 'cors',
-      'content-type': 'application/json',
-      'origin': 'https:....com',
-      'user-agent': 'Mozill...1.15',
-      'referer': 'https:...eea1',
-      'sec-fetch-dest': 'empty',
-    }
-  },
-  {
-    id: 3,
-    requestHeaders: {
-      'host': 'requestbinder.com/3333',
-      'content-length': '97',
-      'accept': 'applic... */*',
-      'sec-fetch-site': 'same-origin',
-      'accept-language': 'en-GB,en;q=0.9',
-      'accept-encoding': 'gzip, deflate, br',
-      'sec-fetch-mode': 'cors',
-      'content-type': 'application/json',
-      'origin': 'https:....com',
-      'user-agent': 'Mozill...1.15',
-      'referer': 'https:...eea1',
-      'sec-fetch-dest': 'empty',
-    }
-  },
-  {
-    id: 4,
-    requestHeaders: {
-      'host': 'requestbinder.com/4444',
-      'content-length': '97',
-      'accept': 'applic... */*',
-      'sec-fetch-site': 'same-origin',
-      'accept-language': 'en-GB,en;q=0.9',
-      'accept-encoding': 'gzip, deflate, br',
-      'sec-fetch-mode': 'cors',
-      'content-type': 'application/json',
-      'origin': 'https:....com',
-      'user-agent': 'Mozill...1.15',
-      'referer': 'https:...eea1',
-      'sec-fetch-dest': 'empty'
-    }
-  },
-]
+// const mongoRequestsData = [
+//   {
+//     id: 1,
+//     requestHeaders: {
+//       'host': 'requestbinder.com/1111',
+//       'content-length': '97',
+//       'accept': 'applic... */*',
+//       'sec-fetch-site': 'same-origin',
+//       'accept-language': 'en-GB,en;q=0.9',
+//       'accept-encoding': 'gzip, deflate, br',
+//       'sec-fetch-mode': 'cors',
+//       'content-type': 'application/json',
+//       'origin': 'https:....com',
+//       'user-agent': 'Mozill...1.15',
+//       'referer': 'https:...eea1',
+//       'sec-fetch-dest': 'empty',
+//     }
+//   },
+//   {
+//     id: 2,
+//     requestHeaders: {
+//       'host': 'requestbinder.com/2222',
+//       'content-length': '97',
+//       'accept': 'applic... */*',
+//       'sec-fetch-site': 'same-origin',
+//       'accept-language': 'en-GB,en;q=0.9',
+//       'accept-encoding': 'gzip, deflate, br',
+//       'sec-fetch-mode': 'cors',
+//       'content-type': 'application/json',
+//       'origin': 'https:....com',
+//       'user-agent': 'Mozill...1.15',
+//       'referer': 'https:...eea1',
+//       'sec-fetch-dest': 'empty',
+//     }
+//   },
+//   {
+//     id: 3,
+//     requestHeaders: {
+//       'host': 'requestbinder.com/3333',
+//       'content-length': '97',
+//       'accept': 'applic... */*',
+//       'sec-fetch-site': 'same-origin',
+//       'accept-language': 'en-GB,en;q=0.9',
+//       'accept-encoding': 'gzip, deflate, br',
+//       'sec-fetch-mode': 'cors',
+//       'content-type': 'application/json',
+//       'origin': 'https:....com',
+//       'user-agent': 'Mozill...1.15',
+//       'referer': 'https:...eea1',
+//       'sec-fetch-dest': 'empty',
+//     }
+//   },
+//   {
+//     id: 4,
+//     requestHeaders: {
+//       'host': 'requestbinder.com/4444',
+//       'content-length': '97',
+//       'accept': 'applic... */*',
+//       'sec-fetch-site': 'same-origin',
+//       'accept-language': 'en-GB,en;q=0.9',
+//       'accept-encoding': 'gzip, deflate, br',
+//       'sec-fetch-mode': 'cors',
+//       'content-type': 'application/json',
+//       'origin': 'https:....com',
+//       'user-agent': 'Mozill...1.15',
+//       'referer': 'https:...eea1',
+//       'sec-fetch-dest': 'empty'
+//     }
+//   },
+// ]
 
-const pgRequestsData = [
-  {
-    id: 1,
-    bin_id: 1,
-    mongo_id: 1,
-    http_method: 'GET',
-    http_path: '/webhook/bin_number/request_number'
-  },
-  {
-    id: 2,
-    bin_id: 2,
-    mongo_id: 2,
-    http_method: 'GET',
-    http_path: '/webhook/bin_number/request_number'
-  },
-  {
-    id: 3,
-    bin_id: 3,
-    mongo_id: 3,
-    http_method: 'GET',
-    http_path: '/webhook/bin_number/request_number'
-  },
-  {
-    id: 4,
-    bin_id: 4,
-    mongo_id: 4,
-    http_method: 'GET',
-    http_path: '/webhook/bin_number/request_number'
-  },
-]
+// const pgRequestsData = [
+//   {
+//     id: 1,
+//     bin_id: 1,
+//     mongo_id: 1,
+//     http_method: 'GET',
+//     http_path: '/webhook/bin_number/request_number'
+//   },
+//   {
+//     id: 2,
+//     bin_id: 2,
+//     mongo_id: 2,
+//     http_method: 'GET',
+//     http_path: '/webhook/bin_number/request_number'
+//   },
+//   {
+//     id: 3,
+//     bin_id: 3,
+//     mongo_id: 3,
+//     http_method: 'GET',
+//     http_path: '/webhook/bin_number/request_number'
+//   },
+//   {
+//     id: 4,
+//     bin_id: 4,
+//     mongo_id: 4,
+//     http_method: 'GET',
+//     http_path: '/webhook/bin_number/request_number'
+//   },
+// ]
 
-const bin = { id: 1, bin_path: 'aabioueriow9343', created_at: '09-13-2023' }
+// const bin = { id: 1, bin_path: 'aabioueriow9343', created_at: '09-13-2023' }
 
-const Header = ({ bin }) => {
+const Header = ({ bin, newBin }) => {
   return (
     <div>
       <h1>Request Inspector</h1>
-      <p>Webhook: www.request-inspect.com/webhook/{bin.bin_path}</p>
-      <button>Create New Inspector</button>
+      <p>Webhook: www.request-inspect.com/webhook/{bin ? bin.bin_path : ""}</p>
+      <button onClick={newBin}>Create New Inspector</button>
     </div>
   )
 }
@@ -157,10 +158,11 @@ const Main = ({ homePage }) => {
 }
 
 const App = () => {
-  const [mongoRequests, setMongoRequests] = useState(mongoRequestsData)
-  const [pgRequests, setPgRequests] = useState(pgRequestsData)
+  const [mongoRequests, setMongoRequests] = useState([])
+  const [pgRequests, setPgRequests] = useState([])
   const [homePage, setHomePage] = useState(null)
-  
+  const [bin, setBin] = useState([])
+
   const handleRequestClick = (mongoID) => {
     console.log(mongoID)
     let requestData = searchMongo(mongoID, mongoRequests)
@@ -176,9 +178,14 @@ const App = () => {
     return singleRequest.requestHeaders;
   }
 
+  const newBin = async () => {
+    let newBin = await requestService.createBin()
+    setBin(newBin)
+  }
+
   return (
     <div>
-      <Header bin={bin} />
+      <Header bin={bin} newBin={newBin} />
       <SideBar pRequests={pgRequests} handleRequestClick={handleRequestClick} />
       <Main homePage={homePage}/>
     </div>
@@ -186,3 +193,15 @@ const App = () => {
 }
 
 export default App;
+
+// useEffect(() => {
+//   contactService
+//     .getAll().then(response => {
+//       setPersons(response)
+//       setFiltered(response)
+//     })
+//     .catch(error => {
+//       alert(`${error}`)
+//       return []
+//     })
+// }, [])
---
+++ frontend/src/services/requests.js
@@ -0,0 +1,122 @@
+// import axios from 'axios'
+// const baseUrl = '/api/contacts'
+
+const mongoRequestsData = [
+  {
+    id: 1,
+    requestHeaders: {
+      'host': 'requestbinder.com/1111',
+      'content-length': '97',
+      'accept': 'applic... */*',
+      'sec-fetch-site': 'same-origin',
+      'accept-language': 'en-GB,en;q=0.9',
+      'accept-encoding': 'gzip, deflate, br',
+      'sec-fetch-mode': 'cors',
+      'content-type': 'application/json',
+      'origin': 'https:....com',
+      'user-agent': 'Mozill...1.15',
+      'referer': 'https:...eea1',
+      'sec-fetch-dest': 'empty',
+    }
+  },
+  {
+    id: 2,
+    requestHeaders: {
+      'host': 'requestbinder.com/2222',
+      'content-length': '97',
+      'accept': 'applic... */*',
+      'sec-fetch-site': 'same-origin',
+      'accept-language': 'en-GB,en;q=0.9',
+      'accept-encoding': 'gzip, deflate, br',
+      'sec-fetch-mode': 'cors',
+      'content-type': 'application/json',
+      'origin': 'https:....com',
+      'user-agent': 'Mozill...1.15',
+      'referer': 'https:...eea1',
+      'sec-fetch-dest': 'empty',
+    }
+  },
+  {
+    id: 3,
+    requestHeaders: {
+      'host': 'requestbinder.com/3333',
+      'content-length': '97',
+      'accept': 'applic... */*',
+      'sec-fetch-site': 'same-origin',
+      'accept-language': 'en-GB,en;q=0.9',
+      'accept-encoding': 'gzip, deflate, br',
+      'sec-fetch-mode': 'cors',
+      'content-type': 'application/json',
+      'origin': 'https:....com',
+      'user-agent': 'Mozill...1.15',
+      'referer': 'https:...eea1',
+      'sec-fetch-dest': 'empty',
+    }
+  },
+  {
+    id: 4,
+    requestHeaders: {
+      'host': 'requestbinder.com/4444',
+      'content-length': '97',
+      'accept': 'applic... */*',
+      'sec-fetch-site': 'same-origin',
+      'accept-language': 'en-GB,en;q=0.9',
+      'accept-encoding': 'gzip, deflate, br',
+      'sec-fetch-mode': 'cors',
+      'content-type': 'application/json',
+      'origin': 'https:....com',
+      'user-agent': 'Mozill...1.15',
+      'referer': 'https:...eea1',
+      'sec-fetch-dest': 'empty'
+    }
+  },
+]
+
+const pgRequestsData = [
+  {
+    id: 1,
+    bin_id: 1,
+    mongo_id: 1,
+    http_method: 'GET',
+    http_path: '/webhook/bin_number/request_number'
+  },
+  {
+    id: 2,
+    bin_id: 2,
+    mongo_id: 2,
+    http_method: 'GET',
+    http_path: '/webhook/bin_number/request_number'
+  },
+  {
+    id: 3,
+    bin_id: 3,
+    mongo_id: 3,
+    http_method: 'GET',
+    http_path: '/webhook/bin_number/request_number'
+  },
+  {
+    id: 4,
+    bin_id: 4,
+    mongo_id: 4,
+    http_method: 'GET',
+    http_path: '/webhook/bin_number/request_number'
+  },
+]
+
+const bin = { id: 1, bin_path: 'aabioueriow9343', created_at: '09-13-2023' }
+
+const createBin = () => {
+  // const request = axios.post(baseURL/newBin)
+  // return request.then(response => response.data)
+  return bin
+}
+
+const getPgRequests = () => {
+  // const request = axios.get(baseUrl) // get from the non-webhook url?
+  // return request.then(response => response.data)
+  return pgRequestsData
+}
+
+const exportable = { getPgRequests, createBin }
+
+export default exportable
