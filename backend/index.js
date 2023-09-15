95330797fa188c357330c37f6b45265f352ed0bd
Leeya Davis <16376036+LeeyaD@users.noreply.github.com>
2023-09-14 20:37:34 -0400

    connected Postgresql
    Co-authored-by: Chris Brum <christopher.brum@gmail.com>
    Co-authored-by: Maddy Woodrum <madzno12@gmail.com>
    Co-authored-by: Spencer Wightman <spencer.wightman@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const Request = require('./models/request')
+const { Client } = require('pg')
+
 // we can call Mongoose methods on Request (I think)
 const app = express()
 
@@ -14,12 +16,23 @@ mongoose.connect('mongodb://localhost:27017/requests')
     console.log('### ERROR CONNECTING TO MongoDB:', error.message)
   });
 
+const connectToPg = () => {
+  const client = new Client({
+    host: 'localhost',
+    database: 'requestInspect',
+    port: 5432,
+  })
+
+  return client
+}
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.post('/test', async (request, response) => {
-  console.log('### TEST CONNECTION');
+// webhook url -- need to match any bin path
+app.post('/webhook', async (request, response) => {
+  console.log('### MONGO CONNECTED -- NEW BIN');
   const stringRequestHeader = JSON.stringify(request.headers);
   const stringRequestBody = JSON.stringify(request.body);
   const req = new Request({
@@ -31,6 +44,16 @@ app.post('/test', async (request, response) => {
   response.send(`<h1>Stringified Request: ${savedRequest}</h1>`)
   console.log('### SAVED REQUEST TO MONGO')
   response.status(201)
+})
+
+app.post('/pgtest', async (request, response) => {
+  const pg = connectToPg()
+  await pg.connect()
+  console.log('### Postgres CONNECTED');
+  pg.query('SELECT $1::text as message', ['Hello world!'])
+    .then(result => console.log(result.rows[0].message)) // Hello world!
+    .then(() => pg.end())
+  response.status(201).send()
 })
 
 const PORT = 3001
